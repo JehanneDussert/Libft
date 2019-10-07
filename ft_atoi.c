0bbad102c5842a48fd8b8dbc15188582adf1460f
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2019-10-07 19:10:32 +0200

    D01

---
+++ ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdussert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 15:24:39 by jdussert          #+#    #+#             */
+/*   Updated: 2019/10/07 18:05:01 by jdussert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_atoi(char *str)
+{
+	int i;
+	unsigned int result;
+	int count;
+
+	i = 0;
+	result = 0;
+	count = 1;
+	while (str[i] != '\0' && (str[i] == '\t' || str[i] == '\n' ||
+				str[i] == '\v' || str[i] == '\f' || str[i] == '\r' ||
+				str[i] == ' '))
+		i++;
+	if (str[i] == '-')
+	{
+		count = -count;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return(result * count);
+}
