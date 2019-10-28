12b0ea646829c704bc0e4e1170f1b06a220c52dd
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2019-10-28 12:18:46 +0100

    Dernier coup de norminette

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jdussert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 15:24:59 by jdussert          #+#    #+#             */
-/*   Updated: 2019/10/28 12:15:38 by jdussert         ###   ########.fr       */
+/*   Updated: 2019/10/28 12:17:54 by jdussert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void		*ft_memmove(void *dst, const void *src, size_t len)
 {
-    if (dst == (char *)src)
-        return (dst);
-    if (dst < src)
-        return (ft_memcpy(dst, src, len));
-    else
-    {
-        while (len)
-        {
-            ((char *)dst)[len - 1] = ((char *)src)[len - 1];
-            len--;
-        }
-    }
-    return (dst);
+	if (dst == (char *)src)
+		return (dst);
+	if (dst < src)
+		return (ft_memcpy(dst, src, len));
+	else
+	{
+		while (len)
+		{
+			((char *)dst)[len - 1] = ((char *)src)[len - 1];
+			len--;
+		}
+	}
+	return (dst);
 }
