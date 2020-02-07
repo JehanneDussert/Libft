ad247c970c0e07b5ca8dec1e6de8aa7af9da984b
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2020-02-07 17:08:43 +0100

    Add ft_last

---
+++ ft_last.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_last.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdussert <jdussert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/07 16:05:49 by jdussert          #+#    #+#             */
+/*   Updated: 2020/02/07 16:33:13 by jdussert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_last(char *haystack, char *needle)
+{
+    int len_h;
+    int len_n;
+
+    len_h = ft_strlen(haystack);
+    len_n = ft_strlen(needle);
+    while (len_n-- > 0 && len_h-- > 0 && haystack[len_h] == needle[len_n])
+        if (len_n == 0)
+            return (1);
+    return (0);
+}
